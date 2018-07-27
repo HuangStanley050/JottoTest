45b37aa2ee8045a2023df30f32ec63ec294ec263
Stanley Huang <infamous_godhand@yahoo.com>
2018-07-27 04:23:31 +0000

    basic tests done for congrats component

--- src/congrats.js
+++ src/congrats.js
@@ -1,5 +1,21 @@
 import React from "react";
 
-export default () => {
-    return <div></div>;
+export default (props) => {
+
+
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+            <span data-test="congrats-message">
+            Congrats! You won.
+            </span>
+            
+            </div>
+        );
+
+    }
+    else {
+        return (<div data-test="component-congrats"/>);
+    }
+
 };
--- src/congrats.test.js
+++ src/congrats.test.js
@@ -11,12 +11,20 @@ const setup = (props = {}) => shallow(<Congrats {...props} />);
 
 describe("Testing for the Congrats component", () => {
     test("renders without error", () => {
-
+        const wrapper = setup();
+        const component = findByTestAtrr(wrapper, 'component-congrats');
+        expect(component.length).toBe(1);
     });
     test("renders no text when \'success\' props is false", () => {
+        const wrapper = setup({ success: false });
+        const component = findByTestAtrr(wrapper, 'component-congrats');
+        expect(component.text()).toBe("");
 
     });
     test("renders non empty congrats message when \'success\' props is true", () => {
+        const wrapper = setup({ success: true });
+        const message = findByTestAtrr(wrapper, 'congrats-message');
+        expect(message.text().length).not.toBe(0);
 
     });
 });
