7eb26433308255fc857ea2a9443d9bc77f01e548
Stanley Huang <infamous_godhand@yahoo.com>
2018-07-27 09:54:26 +0000

    added proptypes testing

--- src/congrats.test.js
+++ src/congrats.test.js
@@ -4,14 +4,22 @@ import EnzymeAdapter from "enzyme-adapter-react-16";
 /*global expect*/
 
 import Congrats from "./congrats";
-import { findByTestAtrr } from "../test/testUtils";
+import { findByTestAtrr, checkProps } from "../test/testUtils";
+//import checkPropTypes from "check-prop-types";
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
-const setup = (props = {}) => shallow(<Congrats {...props} />);
+
+const defaultProps = { success: false };
+
+const setup = (props = {}) => {
+    const setupProps = { ...defaultProps, ...props };
+    return shallow(<Congrats {...setupProps} />);
+
+}
 
 describe("Testing for the Congrats component", () => {
     test("renders without error", () => {
-        const wrapper = setup();
+        const wrapper = setup({ success: false });
         const component = findByTestAtrr(wrapper, 'component-congrats');
         expect(component.length).toBe(1);
     });
@@ -26,5 +34,9 @@ describe("Testing for the Congrats component", () => {
         const message = findByTestAtrr(wrapper, 'congrats-message');
         expect(message.text().length).not.toBe(0);
 
+    });
+    test("Does not throw warning with expected props", () => {
+        const expectedProps = { success: false };
+        checkProps(Congrats, expectedProps);
     });
 });
