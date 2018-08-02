7a8593a1ca453be1e434ed0ab868bed0c671b667
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-02 09:45:16 +0000

    setting up middleware with thunk

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,10 +1,12 @@
 import checkPropTypes from "check-prop-types";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 /*global expect*/
 import rootReducer from "../src/reducers/index";
+import { middlewares } from "../src/configureStore";
 
 export const storeFactory = (initialState) => {
-    return createStore(rootReducer, initialState);
+    const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+    return createStoreWithMiddleware(rootReducer, initialState);
 };
 
 export const findByTestAtrr = (wrapper, val) => {
