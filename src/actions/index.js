861a038c05af054d69c7118548056e2a672e4c9b
Stanley Huang <infamous_godhand@yahoo.com>
2018-08-05 06:45:33 +0000

    start building the guessword reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,17 @@
 export const actionTypes = {
-    CORRECT_GUESS: 'CORRECT_GUESS'
+    CORRECT_GUESS: 'CORRECT_GUESS',
+    GUESS_WORD: 'GUESS_WORD'
 }
 
 /**
  * action creator
  */
-export const correctGuess = () => {
+/*export const correctGuess = () => {
     return { type: actionTypes.CORRECT_GUESS };
+};*/
+
+export const guessWord = (guessedWord) => {
+    return (dispatch, getState) => {
+
+    };
 };
